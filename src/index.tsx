d55e4e1c39cb331ff9b2ed8dae7603af90c45e3d
Dmitry Sternyaev <dimastv@yandex.com>
2021-09-13 12:24:38 +0300

    Testing CSP

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals'; 
 
 ReactDOM.render(
   <React.StrictMode>
@@ -21,3 +21,5 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+console.log(process.env);
